f2b0a40b2f29108e1bf879917dee95715e1376ba
Dean <dean243@hotmail.com>
2021-07-29 11:41:42 +1000

    Added memcmp()

--- kernel/include/memory/Utilities.h
+++ kernel/include/memory/Utilities.h
@@ -2,5 +2,6 @@
 
 #include <stdint-gcc.h>
 
-void memset(void* start, uint8_t value, uint64_t count);
-void memcopy(void* source, void* destination, uint64_t count);
+void memset(void* const start, uint8_t value, uint64_t count);
+void memcopy(const void* const source, void* const destination, uint64_t count);
+int memcmp(const void* const  a, const void* const b, uint64_t count);
--- kernel/memory/Utilities.cpp
+++ kernel/memory/Utilities.cpp
@@ -1,6 +1,6 @@
 #include <memory/Utilities.h>
 
-void memset(void* start, uint8_t value, uint64_t count)
+void memset(void* const start, uint8_t value, uint64_t count)
 {
     for (uint64_t i = 0; i < count; i++)
     {
@@ -8,10 +8,25 @@ void memset(void* start, uint8_t value, uint64_t count)
     } //TODO: lots of optimizations available here
 }
 
-void memcopy(void* source, void* dest, uint64_t count)
+void memcopy(const void* const source, void* const dest, uint64_t count)
 {
-    uint8_t* si = reinterpret_cast<uint8_t*>(source);
+    const uint8_t* const si = reinterpret_cast<const uint8_t* const>(source);
     uint8_t* di = reinterpret_cast<uint8_t*>(dest);
     for (uint64_t i = 0; i < count; i++)
         di[i] = si[i];
+}
+
+int memcmp(const void* const a, const void* const b, uint64_t count)
+{
+    const uint8_t* const ai = reinterpret_cast<const uint8_t* const>(a);
+    const uint8_t* const bi = reinterpret_cast<const uint8_t* const>(b);
+
+    for (int i = 0; i < count; i++)
+    {
+        if (ai[i] > bi[i])
+            return 1;
+        else if (bi[i] > ai[i])
+            return -1;
+    }
+    return 0;
 }
