e79aa5a1fc28686e35c71ce38321d04fa07359d8
Dean Tuckey <dean243@hotmail.com>
2021-07-30 17:15:30 +1000

    Removed using 8259PIC by default

--- kernel/KernelMain.cpp
+++ kernel/KernelMain.cpp
@@ -10,7 +10,6 @@
 #include "KLog.h"
 #include "StringUtil.h"
 #include "Panic.h"
-#include <drivers/8259PIC.h>
 #include <memory/KHeap.h>
 #include <drivers/ACPI.h>
 #include <drivers/APIC.h>
@@ -72,12 +71,6 @@ namespace Kernel
         idtr.SetEntry((void*)InterruptHandlers::GeneralProtectionFault, 0xD, IDT_ATTRIBS_InterruptGate, 0x08);
         idtr.SetEntry((void*)InterruptHandlers::PageFault, 0xE, IDT_ATTRIBS_InterruptGate, 0x8);
 
-        //getting ready for PIC interrupts
-        PIC::Remap();
-        CPU::PortWrite8(PORT_PIC1_DATA, 0b11111101); //2nd bit is keyboard interrupt, enable it
-        CPU::PortIOWait();
-        CPU::PortWrite8(PORT_PIC2_DATA, 0b11111111);
-
         //PIC interrupts
         //idtr.SetEntry((void*)InterruptHandlers::PS2KeyboardHandler, PIC1_IDT_OFFSET + 0x1, IDT_ATTRIBS_InterruptGate, 0x08);
         idtr.SetEntry((void*)InterruptHandlers::TimerHandler, 0x20, IDT_ATTRIBS_InterruptGate, 0x8);
@@ -85,7 +78,7 @@ namespace Kernel
         CPU::LoadIDT(&idtr);
         CPU::EnableInterrupts();
 
-        Log("Interrupt Descriptor Table with offset: 0x", false);
+        Log("IDT with offset: 0x", false);
         Log(ToStrHex(idtr.offset));
     }
 
@@ -120,8 +113,8 @@ extern "C" __attribute__((noreturn)) void KernelMain(BootInfo* bootInfo)
     SetSerialLogging(true);
     Log("COM1 serial initialized. Welcome, user.");
 
-    PrepareInterrupts(bootInfo);
     PrepareDrivers(bootInfo);
+    PrepareInterrupts(bootInfo);
 
     //setup logging for the rest of the boot process (we should do this sooner rather than later)
     Log("Kernel booted.");
