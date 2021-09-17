1ce2aab54330d6795971fb130e718c90a587aebc
Dean <dean243@hotmail.com>
2021-09-17 13:47:54 +1000

    Panic minor tweaks and updated TODO

--- kernel/arch/x86_64/Panic.cpp
+++ kernel/arch/x86_64/Panic.cpp
@@ -1,21 +1,27 @@
 #include <Panic.h>
 #include <KRenderer.h>
 #include <drivers/CPU.h>
+#include <Platform.h>
+
+PLATFORM_REQUIRED(MINOS_PLATFORM_X86_64)
 
 namespace Kernel
 {
     void Panic(const char* reason)
     {
-        KRenderer::The()->Clear(Colour(0xC0000000)); //red background
-        KRenderer::The()->SetCursor(Position(0));
 
-        KRenderer::The()->WriteLine((const char*)"Oops! Kernel panic! :(");
-        KRenderer::The()->WriteLine((const char*)"");
-        KRenderer::The()->WriteLine((const char*)"Reason:");
-        KRenderer::The()->Write((const char*)"    ");
+        //reset display with red background
+        KRenderer::The()->Clear(Colour(0xC0000000));
+        KRenderer::The()->SetCursor({0, 1});
+
+        //emit general description and error message
+        KRenderer::The()->WriteLine("Oops! Kernel panic! :(");
+        KRenderer::The()->WriteLine("");
+        KRenderer::The()->WriteLine("Reason:");
+        KRenderer::The()->Write("    ");
         KRenderer::The()->WriteLine(reason);
 
-        //TODO: display current register state (if we can)
+        //TODO: capture register state (of previous frame) and display it, as well as useful memory/dev info
 
         Drivers::CPU::Halt();
     }
