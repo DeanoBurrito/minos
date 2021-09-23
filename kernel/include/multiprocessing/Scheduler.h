932347b14a1b77e81c7a932415ed85d55fa6999c
Dean Tuckey <dean243@hotmail.com>
2021-09-24 06:59:44 +1000

    Commit to share

--- kernel/include/multiprocessing/Scheduler.h
+++ kernel/include/multiprocessing/Scheduler.h
@@ -1,12 +1,10 @@
 #pragma once
 
 #include <multiprocessing/Thread.h>
-#include <templates/LinkedList.h>
 
 extern "C"
 {
-    //Declaration so we can use it as a function pointer in c++ 
-    void SchedulerTimerInterruptHandler();
+    void scheduler_HandleInterrupt();
 }
 
 namespace Kernel::Multiprocessing
@@ -14,21 +12,11 @@ namespace Kernel::Multiprocessing
     class Scheduler
     {
     private:
-        sl::LinkedList<KernelThread*> threads;
-        KernelThread* currentThread;
 
     public:
         static Scheduler* The();
 
         void Init();
-        //Co-operatively ends the current quantum, and schedules the next task. Does not remove thread from scheduling queue
         void Yield();
-
-        //Called from interrupt handler to 
-        void SelectNextThreadData();
-        
-        void ScheduleThread(KernelThread* thread);
-        void UnscheduleThread(KernelThread* thread);
-        KernelThread* GetCurrentThread();
     };
 }
