f79f34c826d979582d701aeaf257b36b8c1613cf
Dean Tuckey <dean243@hotmail.com>
2021-10-04 22:04:38 +1100

    Added reverse of FloatToBits(), renamed functions

--- syslib/include/Maths.h
+++ syslib/include/Maths.h
@@ -2,31 +2,45 @@
 
 #include <stdint.h>
 
-inline __attribute__((always_inline, unused)) static uint32_t Float32Bits(float arg)
+__attribute__((always_inline, unused)) inline static uint32_t FloatToBits(float arg)
 {
     union { float f; uint32_t i; } un;
     un.f = arg;
     return un.i;
 }
 
-inline __attribute__((always_inline, unused)) static uint64_t Float64Bits(double arg)
+__attribute__((always_inline, unused)) inline static uint64_t DoubleToBits(double arg)
 {
     union { double d; uint64_t i; } un;
     un.d = arg;
     return un.i;
 }
 
+__attribute__((always_inline, unused)) inline static float BitsToFloat(uint32_t arg)
+{
+    union { uint32_t i; float f; } un;
+    un.i = arg;
+    return un.f;
+}
+
+__attribute__((always_inline, unused)) inline static float BitsToDouble(uint64_t arg)
+{
+    union { uint64_t i; double d; } un;
+    un.i = arg;
+    return un.d;
+}
+
 #define IsNan(num) \
 (   \
-    sizeof(num) == sizeof(float) ? (Float32Bits(num) & 0x7FFF'FFFF) > 0x7F80'0000 : \
-    sizeof(num) == sizeof(double) ? (Float64Bits(num) & -1ULL >> 1) > 0x7FFULL << 52 : \
+    sizeof(num) == sizeof(float) ? (FloatToBits(num) & 0x7FFF'FFFF) > 0x7F80'0000 : \
+    sizeof(num) == sizeof(double) ? (DoubleToBits(num) & -1ULL >> 1) > 0x7FFULL << 52 : \
     false \
 )
 
 #define IsInfinite(num) \
 (   \
-    sizeof(num) == sizeof(float) ? (Float32Bits(num) & 0x7FFF'FFFF) == 0x7F80'0000 : \
-    sizeof(num) == sizeof(double) ? (Float64Bits(num) & -1ULL >> 1) == 0x7FFULL << 52 : \
+    sizeof(num) == sizeof(float) ? (FloatToBits(num) & 0x7FFF'FFFF) == 0x7F80'0000 : \
+    sizeof(num) == sizeof(double) ? (DoubleToBits(num) & -1ULL >> 1) == 0x7FFULL << 52 : \
     false \
 )
 
