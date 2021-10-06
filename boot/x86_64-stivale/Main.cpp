3bbf74ecba9974f3de00de9edc8a38ab1fb4373b
Dean Tuckey <dean243@hotmail.com>
2021-10-06 23:51:38 +1100

    commit to share, no serious changes.

--- boot/x86_64-stivale/Main.cpp
+++ boot/x86_64-stivale/Main.cpp
@@ -1,8 +1,15 @@
+#include <stddef.h>
+#include <stdint.h>
 #include "Stivale2.h"
 #include "../BootInfo.h"
+#include "../X86Helpers.h"
 
-extern "C" void StivaleMain()
+extern "C" void StivaleMain(stivale2_struct* stivale)
 {
+    SerialInit();
+
+    Log("Hello stivaleeeee!");
+    
     asm volatile("mov $0xdeadc0de, %rax");
     while (1);
 }
